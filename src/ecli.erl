4b785f74c894858358d520e4ca3229e769093bf1
stwind <stwindfy@gmail.com>
2013-09-18 09:38:56 +0800

    Move macros to hrl file

--- src/ecli.erl
+++ src/ecli.erl
@@ -12,12 +12,7 @@
 -export([connect_node/2]).
 -export([wait_for/1]).
 
--define(PRINT(Fmt), io:format(Fmt)).
--define(PRINT(Fmt, Args), io:format(Fmt, Args)).
-
--define(HALT(Fmt), ?HALT(Fmt, [])).
--define(HALT(Fmt, Args), ?HALT(Fmt, Args, 1)).
--define(HALT(Fmt, Args, Code), halt_with(Fmt, Args, Code)).
+-include("ecli.hrl").
 
 -define(LINE_LENGTH, 75).
 
